38ea503d517bea69f50a49218eb654a5a219f30a
aJTaste <ajtaste-001@outlook.jp>
2026-06-02 23:29:56 +0900

    タイトル間違えてた

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "IroGuessr",
+  title: "IroGuesser",
   description: "表示された色のカラーコードを推測しましょう。",
 };
 
