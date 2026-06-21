b874d083b03c91b1776891f0439bd64496728610
aJTaste <ajtaste-001@outlook.jp>
2026-06-21 19:19:11 +0900

    feat: 今度こそHTML Preview追加

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Page() {
   return (
     <div className="flex flex-col gap-4">
       <Link href="/iro-guesser">IroGuesser</Link>
-      <Link href="/html-preview">HTML Preview</Link>
+      <Link href="/html">HTML Preview</Link>
     </div>
   );
 }
