39a2c90c9ef13d532d6f0a2e5b480f528dabdcfb
Taste <ajtaste-001@outlook.jp>
2026-07-01 10:19:33 +0900

    noteLogic.ts の更新

--- lib/shared/noteLogic.ts
+++ lib/shared/noteLogic.ts
@@ -2,18 +2,18 @@
 import { NoteName, NoteInfo, KeyData } from "@/lib/shared/types";
 
 export const NOTE_NAMES: NoteName[] = [
-  "C",
-  "C#",
-  "D",
-  "D#",
-  "E",
-  "F",
-  "F#",
-  "G",
-  "G#",
-  "A",
-  "A#",
-  "B",
+  "ド",
+  "ド#",
+  "レ",
+  "レ#",
+  "ミ",
+  "ファ",
+  "ファ#",
+  "ソ",
+  "ソ#",
+  "ラ",
+  "ラ#",
+  "シ",
 ];
 
 export const MIN_OCTAVE = 0;
